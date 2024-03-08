e494da96c866af0ebc8497d0d0665bd95acb6f73
jaxe93 <140515954+jaxe93@users.noreply.github.com>
2024-03-08 15:03:59 -0500

    Add files via upload
    added index.js update.

--- index.js
+++ index.js
@@ -1,16 +1,88 @@
-// TODO: Include packages needed for this application
-
 const inquirer = require("inquirer");
-const fs = require("fs");
-const util = require("util");
-// TODO: Create an array of questions for user input
-const questions = [];
+const fs = require('fs');
+const axios = require("axios");
+const generate = require('./utils/generateMarkdown');
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What will the project title be?"
+    },
+    {
+        type: "input",
+        name: "badge",
+        message: "Please provide the badges links that you want"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please provide your project's description"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please provide the installation instructions"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please provide the project usage"
+    },
+    {
+        type: "input",
+        name: "licence",
+        message: "Please provide the project licence or your badge link"
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Please provide the contributing parties"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Please provide the project tests"
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "What is your github user name?"
+    },
+    {
+        type: "input",
+        name: "repo",
+        message: "What is your repo link?"
+    },
+];
 
-// TODO: Create a function to initialize app
-function init() {}
+inquirer
+    .prompt(questions)
+    .then(function(data){
+        const queryUrl = `https://api.github.com/users/${data.username}`;
 
-// Function call to initialize app
+        axios.get(queryUrl).then(function(res) {
+            
+            const githubInfo = {
+                githubImage: res.data.avatar_url,
+                email: res.data.email,
+                profile: res.data.html_url,
+                name: res.data.name
+            };
+            
+          fs.writeFile("README.md", generate(data, githubInfo), function(err) {
+            if (err) {
+              throw err;
+            };
+    
+            console.log("New README file created with success!");
+          });
+        });
+
+});
+
+function init() {
+
+}
+
 init();
